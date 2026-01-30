39b4603f3257b41feb136c17bfada696fb94d03d
Zeynab Jalalian <zjalalianweb@gmail.com>
2026-01-30 20:04:55 +0330

    way

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./App.css"
 import Header from './components/Header'
 import Home from './components/Home'
+import Way from './components/Way'
 
 function App() {
   return (
     <div>
       <Header/>
       <Home/>
+      <Way/>
     </div>
   )
 }
